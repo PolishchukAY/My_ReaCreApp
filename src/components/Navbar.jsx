b6387f42fc8029df830590991d4d8865ad4ac602
Polishchukay <polishchukay@gmail.com>
2020-03-23 17:53:58 +0200

    les014 css module

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import myclases from './Navbar.module.css'
+console.log(myclases);
+/* let s={
+	'nav':'Navbar_nav__30xxxxx'
+	'item':'Navbar_item__3qxxxx'
+	'active':'Navbar_active__xxxxx'
+} */
+
+ /* 
+ <div className="item active"> <a> Profile </a> </div> 
+ */
+let c1="item"
+let c2="active"
+//let classes="item active"
+//let classes=c1+" "+c2;
+//let classes=`${c1} ${c2}`;//шаблонная строка ё тильда
+let classes=`${myclases.item} ${myclases.active}`;//шаблонная строка ё тильда
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div> <a> Profile </a> </div>
-            <div> <a> Messeges </a> </div>
-            <div> <a> News </a> </div>
-            <div> <a> Music </a> </div>
+        <nav className={myclases.nav}>
+            <div className={myclases.item}> <a> Profile </a> </div>
+            <div className={`${myclases.item} ${myclases.active}`}> <a> Messeges </a> </div>
+            <div className={myclases.item}> <a> News </a> </div>
+            <div className={myclases.item}> <a> Music </a> </div>
 			
-            <div> <a> Settings </a> </div>
+            <div className={myclases.item}> <a> Settings </a> </div>
 
         </nav>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import myclases from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className='content'> 
+	<div className={myclases.content}> 
 			
 			<div className='maincontent'>
 				<img src ='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcToFRSPq7mwYz-lLjaJN3DUnlmj7xUIB67s8kdRWUSNuTKCVjGh'/>
@@ -12,15 +13,18 @@ const Profile = () => {
                 ava + discription
 			</div>
 
+			<div>
+                
+			</div>
 
             <div>
 				MyPosts
                 <div>
 					NewPost
 				</div>
-                <div>
-                    <div> post1 </div>
-                    <div> post2 </div>
+                <div className={myclases.posts}>
+                    <div className={myclases.item}> post1 </div>
+                    <div className={myclases.item}> post2 </div>
 				</div>
 
 			</div>
