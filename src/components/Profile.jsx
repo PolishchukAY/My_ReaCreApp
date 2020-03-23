acfd7179fafe3996733487b60bae896bb47f4ce4
Polishchukay <polishchukay@gmail.com>
2020-03-23 14:31:32 +0200

    MyFirstSteps

---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'> 
+			
+			<div className='maincontent'>
+				<img src ='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcToFRSPq7mwYz-lLjaJN3DUnlmj7xUIB67s8kdRWUSNuTKCVjGh'/>
+			</div>
+
+			<div>
+                ava + discription
+			</div>
+
+
+            <div>
+				MyPosts
+                <div>
+					NewPost
+				</div>
+                <div>
+                    <div> post1 </div>
+                    <div> post2 </div>
+				</div>
+
+			</div>
+
+            <div></div>
+            <div></div>
+		</div>
+    )
+}
+export default Profile;
